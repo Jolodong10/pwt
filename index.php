627b49354fe02b0e070fd62d714a0998b1fd1c3c
Jolodong10 <161265102+Jolodong10@users.noreply.github.com>
2024-02-26 08:51:46 +0700

    drag
    Oyacell

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require "layout/head.php"; ?>
+
+<body>
+    <div id="app">
+        <?php require "layout/sidebar.php"; ?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            <div class="page-heading">
+                <h3>Dashboard</h3>
+            </div>
+            <div class="page-content">
+                <section class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 align="Center">PEMILIHAN PROVIDER DENGAN
+                                    METODE SIMPLE ADDITIVE WEIGHTING (SAW)</h4>
+                            </div>
+                            <div class="card-content">
+                                <div class="card-body">
+                                    <p class="card-text" align="justify" style="text-indent: 45px;">
+                                        Pekembangan teknologi di dunia Provider sangat dibutuhkan untuk
+                                        menunjang aktivitas masyarakat, Provider merupakan kartu yang kita
+                                        gunakan untuk berlangganan pada satu operator seluler. Di Indonesia sendiri
+                                        banyak Provider yang menyediakan layanan dan juga kemudahan dalam
+                                        mengakses internet dengan berbagai macam kriteria yang ditawarkan masing-masing
+                                        operator, konsumen dituntut untuk melihat manakah Provider yang sesuai
+                                        dengan kebutuhan.
+                                    </p>
+                                    <p class="card-text" align="justify" style="text-indent: 45px;">
+                                        Permasalah adanya persaingan provider, konsumen dituntut menjadi jeli dengan
+                                        kebutuhan dan harga jualnya sehingga tidak merugikan konsumen itu sendiri. Hal
+                                        ini sudah tentu akan menyulitkan konsumen dalam pemilihan provider yang tepat
+                                        sesuai dengan kriteria yang dibutuhkan. Untuk mengatasi masalah pemilihan
+                                        provider tersebut maka akan dibuat pendukung keputusan dengan menggunakan metode
+                                        Simple Additive Weighting (SAW), yang diharapkan dapat membantu konsumen dalam
+                                        menentukan provider mana yang akan dipilih sesuai dengan kebutuhan. Penelitian
+                                        ini mempunyai 5 kriteria yaitu sinyal, koneksi internet paket internet, masa
+                                        aktif dan harga. Kemudian mempunyai dua alternatif yaitu Telkomsel, Indosat, dan
+                                        Smartfren.
+                                    </p>
+                                    <p class="card-text" align="justify" style="text-indent: 45px;">
+                                        Metode penelitian ini menggunakan kuisioner yang dilakukan dengan memberikan
+                                        kuisioner terhadap responden yang telah ditentukan, dimana responden disini
+                                        adalah penilai yang berhak menilai provider mana yang terpilih dari penentuan 5
+                                        (lima kriteria yang telah ditentukan) yaitu Sinyal, Koneksi Internet, Paket
+                                        Internet, Masa Aktif, Harga. Adapun alternatif yang digunakan yaitu provider
+                                        Telkomsel, Indosat, Smartfren. Dalam penelitian ini penulis menyebarkan 60
+                                        kuisioner dimana kuesioner ini diisi oleh konsumen atau pelanggan disebarkan
+                                        kepada konsumen berjumlah 60, setiap konsumen mengisi satu kuisioner yang telah
+                                        di rancang sendiri oleh penulis.
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <?php require "layout/footer.php"; ?>
+        </div>
+    </div>
+    <?php require "layout/js.php"; ?>
+</body>
+
+</html>
---
+++ layout/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <span>Copyright &copy; OYA CELL <?= date('Y') ?></span>
+        </div>
+    </div>
+</footer>
